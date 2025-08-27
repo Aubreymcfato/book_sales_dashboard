2450a53e95572aeb96f079d3cf1f4d118f3381e7
Andrea <aubreymcfato@gmail.com>
2025-08-27 16:08:29 +0200

    voglio la heatmap come prima

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px  # Switch to Plotly for better full-screen interactivity
+import altair as alt  # Per la heatmap
+import plotly.express as px  # Per i top charts e linee
 import os
 import glob
 import re
